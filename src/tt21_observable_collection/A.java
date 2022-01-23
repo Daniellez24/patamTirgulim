df182e79f348dfea388fa8c0820f8753b6d4c5a8
Tamir <1tamir198@gmail.com>
2022-01-23 15:06:36 +0200

    Adding observable collection

---
+++ src/tt21_observable_collection/A.java
@@ -0,0 +1,23 @@
+package tt21_observable_collection;
+
+import java.util.Observable;
+
+/**Data stractur that will notify us when some things changed*/
+public class A extends Observable{
+    int x;
+
+    public A(int x) {
+        this.x = x;
+    }
+
+    public int get() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+        setChanged();
+        notifyObservers();
+
+    }
+}
---
+++ src/tt21_observable_collection/ObservableCollection.java
@@ -0,0 +1,47 @@
+package tt21_observable_collection;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.function.Consumer;
+
+public class ObservableCollection<E> implements Observer {
+
+    private  Collection<E> collection;
+    private Consumer<Collection<E>> consumer;
+
+    public ObservableCollection(Collection<E> collection) {
+        this.collection = collection;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        notifyChanges();
+    }
+
+    public void add(E e) {
+        if(e instanceof Observable){
+            Observable o = (Observable)e;
+            o.addObserver(this);
+        }
+        collection.add(e);
+        notifyChanges();
+    }
+
+    public void remove(E e) {
+        collection.remove(e);
+        notifyChanges();
+    }
+
+    private void notifyChanges(){
+        if(consumer != null){
+            consumer.accept(collection);
+        }
+    }
+
+    public void onChange(Consumer<Collection<E>> consumer) {
+        this.consumer = consumer;
+
+    }
+}
